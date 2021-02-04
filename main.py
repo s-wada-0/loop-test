5b43b59a0417693c53aaab301d08d9e3bdb7e6f9
s-wada-0 <shunsuke.wada.0@gmail.com>
2021-02-04 12:46:39 +0900

    add drawStickFig

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import eel
 import time
 import random
 
+list = [[600, 150], [550, 200], [500, 250], [450, 300], [650, 200], [700, 250], [750, 300], [575, 275], [550, 350], [525, 425], [625, 275], [650, 350], [675, 425] ]
+
 @eel.expose
 def analyze(time):
-    return random.randint(0,100)
+    score = random.randint(0,100)
+    for i in range(13):
+        for j in range(2):
+            list[i][j] += random.randint(-3, 3)
+
+
+    val = {"score": score, "landmark": list}
+    return val
 
 eel.init("web")
 eel.start("loop-test.html") #first page
--- web/loop-test.js
+++ web/loop-test.js
@@ -31,21 +31,70 @@ function playing(){
             }else if(getCurrentTime()==-2){
                 clearInterval(id);
             }
-        }, 1000); //1000ms ごとにpython側へ動画の再生時間の情報を送る
+        }, 50); //50ms ごとにpython側へ動画の再生時間の情報を送る
     }
 }
 
 async function commToPy() {
     //console.log("test");
     let val = await eel.analyze(getCurrentTime)();
-    console.log(val + " from Python");
+    //console.log(val + " from Python");
 
-    drawMatchRate(val);
+    deleteCanvas();
+    drawMatchRate(val.score);
+    drawStickFig(val.landmark);
+}
+
+function deleteCanvas(){
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
 }
 
 //val(得点) に応じてゲージ的なものを描画
 function drawMatchRate(val){
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(10, 100-val+10, 50, val);
+}
+
+function drawStickFig(val){
+
+    for(var i=0;i<13;i++){
+        if(val[i][0]>=0 && val[i][1]>=0){
+            ctx.beginPath();
+            ctx.arc(val[i][0], val[i][1], 10, 0, Math.PI*2, false);
+            ctx.fill();
+        }
+    }
+
+    //ryou kata no juusin
+    if(val[1][0]>=0 && val[4][0]>=0 && val[1][1]>=0 && val[4][1]>=0){
+        var juusin = [(val[1][0]+val[4][0])/2, (val[1][1]+val[4][1])/2];
+    }else{
+        var juusin = [-1,-1];
+    }
+
+    drawLine(val[0], juusin);
+    drawLine(val[1], val[4]);
+    drawLine(val[1], val[2]);
+    drawLine(val[2], val[3]);
+    drawLine(val[4], val[5]);
+    drawLine(val[5], val[6]);
+    drawLine(juusin, val[7]);
+    drawLine(val[7], val[8]);
+    drawLine(val[8], val[9]);
+    drawLine(juusin, val[10]);
+    drawLine(val[10], val[11]);
+    drawLine(val[11], val[12]);
+
+}
+
+function drawLine(p1, p2){
+    if(p1[0]<0 || p1[1]<0 || p2[0]<0 || p2[1]<0){
+        return false;
+    }
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "aliceblue";
+    ctx.moveTo(p1[0], p1[1]);
+    ctx.lineTo(p2[0],p2[1]);
+    ctx.stroke();
 }
